5be395b88c0766ab80daca9eae49fe28e4d46c93
= <hardlane2000@gmail.com>
2021-11-30 17:59:14 +0100

    committing before upload

--- app.py
+++ app.py
@@ -2,11 +2,15 @@ from flask import Flask, render_template, request, redirect, url_for, jsonify, s
 from books import *
 from movies import *
 
+import routing.booksroutes
+
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = "secretkey"
 
 all_books = [i.__dict__ for i in all_books]
+
+app.add_url_rule('/', view_func=routing.booksroutes.index)
 
 @app.route("/api")
 def api():
